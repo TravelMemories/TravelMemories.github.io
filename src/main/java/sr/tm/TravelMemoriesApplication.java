50da1b02f49f3ae8fc7ba8c0ae58242c1b3e5d91
Dawid Grabowski <92030397+dxrgrabowski@users.noreply.github.com>
2024-01-10 16:35:50 +0000

    Revert "Merge remote-tracking branch 'origin/Backend' into Frontend+Backend"
    This reverts commit f001af24a288ef877c4f5408ff5c105b53c5dd0a, reversing
    changes made to 8f918d333f0c26e4b4fd39b366e0b0a98cb9aa78.

--- src/main/java/sr/tm/TravelMemoriesApplication.java
+++ src/main/java/sr/tm/TravelMemoriesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TravelMemoriesApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TravelMemoriesApplication.class, args);
 	}
