b1993f28e4fd3067b076718b73f7abbd808ea8eb
Dawid Grabowski <92030397+dxrgrabowski@users.noreply.github.com>
2024-01-10 16:41:53 +0000

    remove merge bug v2

--- frontend/src/pages/PublicMemoriesPage.tsx
+++ frontend/src/pages/PublicMemoriesPage.tsx
@@ -22,20 +22,3 @@ function PublicMemoriesPage() {
 }
 
 export default PublicMemoriesPage;
-import React from "react";
-import { useUserContext } from "../context/UserContext";
-import Navbar from "../components/navbar/Navbar";
-
-function PublicMemoriesPage() {
-  const { isLoggedIn } = useUserContext();
-  return (
-    <>
-      {!isLoggedIn && <Navbar />}
-      <div className="w-full min-h-[100vh] flex items-center justify-center">
-        PublicMemoriesPage
-      </div>
-    </>
-  );
-}
-
-export default PublicMemoriesPage;
